33fee63b138c8b84b8346b33f6c9107b5c126c5e
Jude Ragudo <60352017+jude-ragudo@users.noreply.github.com>
2024-12-21 16:50:47 +0800

    Add files via upload

---
+++ tbgi-inc-mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace tbgi_inc_mvc
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
---
+++ tbgi-inc-mvc/App_Start/RouteConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace tbgi_inc_mvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Technology",
+                url: "Technology",
+                defaults: new { controller = "Home", action = "Technology" }
+            );
+
+            routes.MapRoute(
+                name: "Products",
+                url: "Products",
+                defaults: new { controller = "Home", action = "Products" }
+            );
+
+            routes.MapRoute(
+                name: "DistributionNetwork",
+                url: "DistributionNetwork",
+                defaults: new { controller = "Home", action = "DistributionNetwork" }
+            );
+            
+            routes.MapRoute(
+                name: "Services",
+                url: "Services",
+                defaults: new { controller = "Home", action = "Services" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
